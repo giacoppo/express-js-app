d2feaa1f754387a292dc837c345d5a6b35b82051
giacoppo <giacoppo@gmail.com>
2017-11-07 16:21:05 +0100

    implementing socket.io

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
+var io = require('socket.io')();
 
 app.set('port', process.env.PORT || 3000);
 app.set('appData', dataFile);
@@ -16,9 +17,20 @@ app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
 app.use(require('./routes/feedback'));
 app.use(require('./routes/api'));
+app.use(require('./routes/chat'));
 
 var server = app.listen(app.get('port'), function() {
    console.log('Listening on port ' + app.get('port'));
+});
+
+io.attach(server);
+
+io.on('connection', function(socket) {
+    //console.log('User Connected');
+    socket.on('postMessage', function(data) {
+      //console.log('User Disconnected');
+        io.emit('updateMessages', data);
+    });
 });
 
 reload(server, app);
